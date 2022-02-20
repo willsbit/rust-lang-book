ddcdf078fc8177057131795f2bdb0235fb643c72
Willian <willsbit@gmail.com>
2022-02-19 23:41:20 -0300

    3.1. Variáveis e mutabilidade

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\willi\Desktop\2022\Cursos\Rust\rust-lang-book\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\willi\Desktop\2022\Cursos\Rust\rust-lang-book\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\willi\Desktop\2022\Cursos\Rust\rust-lang-book\variables\target\debug\variables.exe: C:\Users\willi\Desktop\2022\Cursos\Rust\rust-lang-book\variables\src\main.rs
